d75e17ea6f5d7870ef4ebf5701851c4b1342b3db
david.mise <davidkingmushi@gmail.com>
2023-11-07 10:50:30 +0300

    admin login/out page

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,88 +1,85 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <h1> Admin Dashboard</h1>
-    <p>Choose your favorite Web language:</p>
-
-<form>
-    <p>radio</p>
-  <input type="radio" id="html" name="fav_language" value="HTML">
-  <label for="html">HTML</label><br>
-  <input type="radio" id="css" name="fav_language" value="CSS">
-  <label for="css">CSS</label><br>
-  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-  <label for="javascript">JavaScript</label> <br>
-
-  <p>checkbox</p>
-  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-  <label for="vehicle1"> I have a bike</label><br>
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-  <label for="vehicle2"> I have a car</label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-  <label for="vehicle3"> I have a boat</label><br>
-
-  <p>textarea</p>
-<textarea name="" id="" cols="30" rows="10">
-    dummy data
-</textarea> <br>
-
-<input type="button" onclick="alert('Hello World!')" value="Click Me!">
-</form>
-</body>
-</html> -->
-
 <!DOCTYPE html>
+<!--
+Template Name: NobleUI - HTML Bootstrap 5 Admin Dashboard Template
+Author: NobleUI
+Website: https://www.nobleui.com
+Portfolio: https://themeforest.net/user/nobleui/portfolio
+Contact: nobleui123@gmail.com
+Purchase: https://1.envato.market/nobleui_admin
+License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
+-->
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .animated-icon {
-            position: fixed;
-            top: 50px;
-            left: 20%;
-            transform: translateX(100%);
-            transition: top 1s;
-            z-index: 999;
-        }
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="NobleUI">
+	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-        /* Additional styles for the icon */
-        .fa-arrow-down {
-            color: #007bff;
-        }
-    </style>
+	<title>Admin Panel - Realestate</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
+
+	<!-- core:css -->
+	<link rel="stylesheet" href="{{ asset('../assets/vendors/core/core.css') }}">
+	<!-- endinject -->
+
+	<!-- Plugin css for this page -->
+	<link rel="stylesheet" href="{{ asset('../assets/vendors/flatpickr/flatpickr.min.css') }}">
+	<!-- End plugin css for this page -->
+
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('../assets/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('../assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<!-- endinject -->
+
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="{{ asset('../assets/css/demo2/style.css') }}">
+  <!-- End layout styles -->
+
+    <link rel="shortcut icon" href="{{ asset('../assets/images/favicon.png') }}" />
 </head>
 <body>
-    <div class="container mt-5">
-        <!-- Your page content here -->
-        <h1>Welcome to admin dashboard</h1>
-        <p>Scroll down to see the animated icon</p>
-        <div style="height: 2000px;"></div>
+    <div class="main-wrapper">
+                <!-- partial:partials/_sidebar.html -->
+        @include('admin.body.sidebar')
+                <!-- partial -->
+            
+        <div class="page-wrapper">
+                    <!-- partial:partials/_navbar.html -->
+
+                 @include('admin.body.header')
+                 <!-- partial -->
+                 @yield('admin')
+                    <!-- partial:partials/_footer.html -->
+                 @include('admin.body.footer')
+                   
+                    <!-- partial -->
+        </div>
     </div>
 
-    <div class="animated-icon" id="animatedIcon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
-            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
-        </svg>
-    </div>
+	<!-- core:js -->
+	<script src="{{ asset('../assets/vendors/core/core.js') }}"></script>
+	<!-- endinject -->
 
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script> <!-- Font Awesome CDN for icons -->
-    <script>
-        window.onscroll = function() { scrollFunction() };
+	<!-- Plugin js for this page -->
+    <script src="{{ asset('../assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('../assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+        <!-- End plugin js for this page -->
 
-        function scrollFunction() {
-            const icon = document.getElementById('animatedIcon');
-            const scrollPosition = window.pageYOffset;
+    <!-- inject:js -->
+    <script src="{{ asset('../assets/vendors/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('../assets/js/template.js') }}"></script>
+    <!-- endinject -->
 
-            icon.style.top = 200 + scrollPosition * 1 + 'px'; /* Adjust the multiplier for different speeds */
-        }
-    </script>
+    <!-- Custom js for this page -->
+    <script src="{{ asset('../assets/js/dashboard-dark.js') }}"></script>
+        <!-- End custom js for this page -->
+
 </body>
-</html>
+</html>    
--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,16 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+    ->name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])
+    ->name('admin.logout');
 }); //end Group Admin Middleware
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 }); //end Group Agent Middleware
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])
+->name('admin.login');
