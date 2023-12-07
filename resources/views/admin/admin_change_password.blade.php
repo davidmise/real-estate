8aa08b641708257c5115cdb99aba656415074e9a
davidmise <davidkingmushi@gmail.com>
2023-12-07 15:17:56 +0300

    admin Profile Chaange Password Pt1

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -54,14 +54,35 @@
         <div class="row">
           <div class="card">
             <div class="card-body">
-              <h6 class="card-title">Update Admin Profile</h6>
+              <h6 class="card-title"> Admin Change Password</h6>
 
               <form method="POST" action="{{route('admin.profile.store')}}" class="forms-sample" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
-                    <label for="exampleInputUsername1" class="form-label">Name</label>
-                    <input type="text" name="name" class="form-control" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->name }}">
+                    <label for="exampleInputUsername1" class="form-label">Old Password</label>
+                    <input type="password" name="old_password" class="form-control
+                    @error('old_password') is-invalid @enderror " id="old_password" autocomplete="off">
+                    @error('old_password')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputUsername1" class="form-label">New Password</label>
+                    <input type="password" name="new_password" class="form-control
+                    @error('new_password') is-invalid @enderror " id="new_password" autocomplete="off">
+                    @error('new_password')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputUsername1" class="form-label">Confirm New Password</label>
+                    <input type="password" name="new_password_confirmation" class="form-control
+                     id="new_password_confirmation" autocomplete="off">
+
+                </div>
+
 
                 <button type="submit" class="btn btn-primary me-2">Save Changes</button>
               </form>
